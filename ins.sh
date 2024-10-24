e1d459a6d0b633c7e8046058b8ecf39a671c9bd6
sedasdas <qq1820029262@gmail.com>
2024-10-24 19:03:37 +0800

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -34,3 +34,5 @@ chmod +x start_gost
 systemctl daemon-reload
 systemctl restart gost.service
 systemctl enable gost.service
+sudo fallocate -l 1G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo reboot
